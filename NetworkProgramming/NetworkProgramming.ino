950f7d506fcd92f9879fed13dc21f18c3626b976
Glen-tech <11800025@student.pxl.be>
2020-04-28 17:20:08 +0200

    Toevoeging sensor
    - Temp sensor werkt
    - Ventilator aansturen lijkt me beter via de raspery pi
    - Sd kaart nog niet toegekomen

--- NetworkProgramming/NetworkProgramming.ino
+++ NetworkProgramming/NetworkProgramming.ino
@@ -1,15 +1,26 @@
 #include <ESP8266WiFi.h>
+#include <Wire.h>
+#include "Adafruit_MCP9808.h"
+ 
+// Create the MCP9808 temperature sensor object
+Adafruit_MCP9808 tempsensor = Adafruit_MCP9808();
+
+long duration;
+int distance;
+int ventilator = 4;
 
 char ssid[]    = "telenet-apn-a26e6";
 char password[] = "2yao18psxaqd";
 IPAddress host(193,190,154,184); 
-String Data = "Testing message to BenEthernet from ESP32";
-DHT dht(DHTPIN, DHTTYPE);
+String Data;
+void temperatuur();
 
 void setup() {
+
+  pinMode(ventilator, OUTPUT);    
   Serial.begin(9600);
+
   WiFi.begin(ssid, password);
- 
   Serial.println("Verbinding aan het maken");
   
   while(WiFi.status() != WL_CONNECTED)
@@ -19,14 +30,36 @@ void setup() {
   }
   Serial.println();
   Serial.print("Connected, IP address: ");
-  Serial.println(WiFi.localIP()); // locale IP van de ESP32
+  Serial.println(WiFi.localIP()); // locale IP van de ESP32*/
+
+  if (!tempsensor.begin())
+  {
+    Serial.println("Couldn't find MCP9808!");
+    while (1);
+  }
+
 }
 
 void loop() 
 {
-    WiFiClient client;
-  
-    if (!client.connect( host , 24041))
+  WiFiClient client;  
+  float c = tempsensor.readTempC();
+  Serial.print("Temp: ");
+  Serial.print(c);
+  Serial.print(" C\t"); 
+
+    if(c > 28.00)
+    {
+      digitalWrite(ventilator,HIGH);
+      Serial.print("Ventilator staat aan");
+    }
+
+    else 
+    {
+      digitalWrite(ventilator,LOW);
+      Serial.print("Ventilator staat uit");
+    }
+   if (!client.connect( host , 24041))
     {      
         Serial.println("Connection to host failed");
         delay(1000);
@@ -34,19 +67,21 @@ void loop()
     }
          else
          {
-            Serial.println("Temperatuur is ");
-            Serial.print(t);
-            Serial.println("connecting...");
-            client.write((uint8_t)1);
-            client.write((uint8_t)0);
-            client.write((uint8_t)(Data.length() + 1));
-            client.write((uint8_t)0);
-        
-            // here's the actual content of the PUT request:
-            client.print(Data);
-            client.flush();
-            client.stop();
-            delay(1000);          
+                    
+                  Data = String(c);
+                  Serial.println("Temperatuur is ");
+                  Serial.print(c);
+                  Serial.println("connecting...");
+                  client.write((uint8_t)1);
+                  client.write((uint8_t)0);
+                  client.write((uint8_t)(Data.length() + 1));
+                  client.write((uint8_t)0);
+                  // here's the actual content of the PUT request:
+                  client.print(Data);
+                  client.flush();
+                  client.stop();
+                  delay(3000);       
          }
   delay(500);
   }
+  
