94f555588bf59cd5d734cdbd8b33eb2384228aed
Glen-tech <11800025@student.pxl.be>
2020-08-24 09:47:24 +0200

    Revert "Examen , kan beter"
    This reverts commit 53dad8d3dbf4c654d96e9d124c162722c6c9edb6.

--- client.cpp
+++ client.cpp
@@ -17,38 +17,22 @@ int main ()
     zmq::context_t context(1);
     zmq::socket_t push (context,ZMQ_PUSH);
 	push.connect("tcp://benternet.pxl-ea-ict.be:24041");
-	
-	zmq::socket_t sub (context, ZMQ_SUB);
-	sub.connect("tcp://benternet.pxl-ea-ict.be:24042");
 
     cout << "Welkom bij project netwerken" <<endl;
 	char geefin[50];
-	cout << "Geef Data>ESPbureau>temp> voor temperatuur van de bureau " << endl;
-	cout << "Geef Data>ESPbureau>licht> in voor het licht in de bureau " <<endl;
-	cout << "Geef Data>AllData>ESP> in voor alla data te ontvangen" << endl;
-	
-	const char *filter  = "Temperatuur>Delta>Bureau";
-
-	sub.setsockopt(ZMQ_SUBSCRIBE, filter,  strlen(filter));
-	
+	cout << "Geef Data>ESPbureau>temp> voor temperatuur van de bureau op de 2de verdieping.\n\rGeef Data>ESPbureau>Licht in voor het licht in de bureau van de 2de verdieping."<<endl;
+	cout << "Geef Data>ESPliving>temp> voor temperatuur van de living op de gelijksvloers."<<endl;
      while (true) 
 	 {
-		cin.getline(geefin,sizeof(geefin));
-		zmq::message_t request (strlen(geefin));
-		memcpy (request.data(), &geefin,strlen(geefin));
-		
+	    cin.getline(geefin,sizeof(geefin));
+        zmq::message_t request (strlen(geefin));
+        memcpy (request.data(), &geefin,strlen(geefin));
+
         push.send(request);
 		string rp2 = std::string(static_cast<char*>(request.data()), request.size());
         sleep(1);
 		
 		memcpy (geefin,"",50); 
-		
-				
-		zmq::message_t delta;
-        sub.recv(&delta);
-	    string data  = std::string(static_cast<char*>(delta.data()), delta.size());	
-		cout << data << endl;	
-		
         
      }
     return 0;
